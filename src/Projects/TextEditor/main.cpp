cc3bfa769afb81b95296dbb674d59513d43ef5e3
jiguotong <1776220977@qq.com>
2023-05-05 20:22:08 +0800

    [Update]更新

--- src/Projects/TextEditor/main.cpp
+++ src/Projects/TextEditor/main.cpp
@@ -4,14 +4,13 @@
 #include <QMessageBox>
 #include <QTime>
 #include <windows.h>
-
+#define ZY 
 int main(int argc, char *argv[])
 {
-    
     QApplication a(argc, argv);
 
     /*时间有效期为3天，三天后不可用！*/
-    QDateTime baseTime = QDateTime::fromString("2023-05-02 00:00:00", "yyyy-MM-dd hh:mm:ss");       // 规定一个初始化基准时间
+    QDateTime baseTime = QDateTime::fromString("2023-05-04 00:00:00", "yyyy-MM-dd hh:mm:ss");       // 规定一个初始化基准时间
     QDateTime currentTime = QDateTime::currentDateTime();                                           //获取系统当前的时间
     int startTime = baseTime.toTime_t();        //将当前时间转为时间戳
     int endTime = currentTime.toTime_t();       //将当前时间转为时间戳
@@ -24,8 +23,13 @@ int main(int argc, char *argv[])
     for (size_t i = 0; i < argc; i++){
         printf("argv[%d]:%s\n",i, argv[i]);
     }
+    
+#ifdef ZY
+    QPixmap pixmap(":/textEditor/res/splash_zy.png");   //设置启动画面
+#else
     //设置开机启动动画
     QPixmap pixmap(":/textEditor/res/splash.png");   //设置启动画面
+#endif // ZY
     pixmap = pixmap.scaled(250, 250, Qt::KeepAspectRatio);
     QSplashScreen splash(pixmap);
     splash.show();   //显示此启动图像
